f1b1502247cdd128b07a46ef5ec502673105973b
phantruongazapi <trungtruongphan116@gmail.com>
2025-03-04 07:03:12 +0700

    Create UserScript.user.js

---
+++ UserScript.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Auto Add Friends (FB & IG)
+// @namespace    http://tampermonkey.net/
+// @version      4.9
+// @description  Tự động thêm bạn bè
+// @author       script by phantruong
+// @require      
+// @updateURL    
+// @downloadURL  
+// @match        *://www.facebook.com/*
+// @match        *://m.facebook.com/*
+// @match        *://www.instagram.com/*
+// @match        *://mobile.instagram.com/*
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @run-at       document-end
+// ==/UserScript==
